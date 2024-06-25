180e6075ad535063b2ccbfe078fa0e1baf08c5e3
Ariel <ariel.quezada06@inacapmail.cl>
2024-06-24 21:37:31 -0400

    Un numero fijo en el inicio equisde

--- app/src/main/java/com/example/techfood/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/techfood/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Inicio"
+        value = "Numero de Atencion"
     }
     val text: LiveData<String> = _text
 }
